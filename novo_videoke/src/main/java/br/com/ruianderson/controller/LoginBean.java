05a0ab2ba6b7f3a01d2cc0113a28cefe96061631
ruiandersonsantos <ruiandersonsantos@gmail.com>
2015-12-09 17:12:03 -0200

    Desenvolvida a funcionalidade de alterar senha do participante.

--- novo_videoke/src/main/java/br/com/ruianderson/controller/LoginBean.java
+++ novo_videoke/src/main/java/br/com/ruianderson/controller/LoginBean.java
@@ -16,6 +16,7 @@ import br.com.ruianderson.modelo.Login;
 import br.com.ruianderson.modelo.Participante;
 import br.com.ruianderson.service.LoginService;
 import br.com.ruianderson.service.NegocioException;
+import br.com.ruianderson.service.ParticipanteService;
 import br.com.ruianderson.util.cdi.CDIServiceLocator;
 import br.com.ruianderson.util.jsf.FacesUtil;
 
@@ -26,11 +27,17 @@ public class LoginBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private LoginService loginservice;
+	private ParticipanteService participanteservice;
 	
 	private Login login;
 	
+	private String senha;
+	private String novasenha;
+	private String confirmasenha;
+	
 	public LoginBean(){
 		this.loginservice = CDIServiceLocator.getBean(LoginService.class);
+		this.participanteservice = CDIServiceLocator.getBean(ParticipanteService.class);
 	}
 	
 	public String logar(){
@@ -48,6 +55,46 @@ public class LoginBean implements Serializable{
 			FacesUtil.addErrorMessage(e.getMessage());
 			return null;
 		}
+	}
+	
+	public String alterarSenhaParticipante() {
+		// pegando o objeto login da sessão.
+		Login objlogin = (Login) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuariologado"); 
+		
+		if(objlogin != null){
+			//Subscrevo a senha do objeto pela senha digitada 
+			objlogin.setSenha(senha);
+			
+			try {
+				Participante objparticipante = this.loginservice.autenticarParticipante(objlogin);
+				if(objparticipante != null){
+					
+					if(novasenha.trim().equals(confirmasenha.trim())){
+						objparticipante.setSenha(novasenha);
+						
+						this.participanteservice.salvar(objparticipante);
+						
+						FacesUtil.addSuccessMessage("Nova senha cadastrada com sucesso!");
+						
+						this.confirmasenha = "";
+						this.novasenha = "";
+						this.senha = "";
+					}
+					
+				}else{
+					FacesUtil.addErrorMessage("Senha atual não confere!");
+				}
+				
+			} catch (NegocioException e) {
+				// TODO Auto-generated catch block
+				FacesUtil.addErrorMessage("Senha atual não confere!");
+			}
+			
+		}
+		
+		
+		return "";
+		
 	}
 	
 	public String logarParticipante(){
@@ -69,14 +116,24 @@ public class LoginBean implements Serializable{
 	}
 	
 	public String sair(){
+		
+		String retorno = "";
+		
 		Login login = (Login) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuariologado");
 		this.loginservice.sairSistema();
+		FacesUtil.addSuccessMessage("Obrigado por usar nosso sistema!");
+		if(login.getParticipante() == null){
+			retorno = "/index.xhtml?faces-redirect=true";
+		}else{
+			
+			retorno = "/mobile/loginp2.xhtml?faces-redirect=true";
+		}
 		
-		FacesUtil.addSuccessMessage("Obrigado por usr nosso sistema!");
+		
 		
 		this.limpar();
 		
-		return "/index.xhtml?faces-redirect=true";
+		return retorno;
 	}
 	
 	
@@ -97,5 +154,30 @@ public class LoginBean implements Serializable{
 	public void setLogin(Login login) {
 		this.login = login;
 	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNovasenha() {
+		return novasenha;
+	}
+
+	public void setNovasenha(String novasenha) {
+		this.novasenha = novasenha;
+	}
+
+	public String getConfirmasenha() {
+		return confirmasenha;
+	}
+
+	public void setConfirmasenha(String confirmasenha) {
+		this.confirmasenha = confirmasenha;
+	}
+	
 	
 }
